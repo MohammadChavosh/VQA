8f4918a63e312309e835c3a9fc0513ddd6b4bbc1
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-06-24 02:48:52 +0430

    test restore resnet

--- restore_resnet.py
+++ restore_resnet.py
@@ -17,8 +17,8 @@ for i in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='scale5'):
 # # Now, let's access and create placeholders variables and
 # # create feed-dict to feed new data
 #
-# graph = tf.get_default_graph()
-# w1 = graph.get_tensor_by_name("w1:0")
+graph = tf.get_default_graph()
+w1 = graph.get_tensor_by_name("scale5/x")
 # w2 = graph.get_tensor_by_name("w2:0")
 # feed_dict ={w1:13.0,w2:17.0}
 #
