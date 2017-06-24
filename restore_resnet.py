428eebd95164e7c39db211ad19ee64f940233181
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-06-24 15:16:53 +0430

    refactor

--- restore_resnet.py
+++ restore_resnet.py
@@ -24,16 +24,6 @@ saver.restore(sess, 'data/tensorflow-resnet-pretrained-20160509/ResNet-L152.ckpt
 graph = tf.get_default_graph()
 images = graph.get_tensor_by_name("images:0")
 img = load_image("data/train2014/COCO_train2014_000000000009.jpg")
-i = [
-	# graph.get_tensor_by_name("scale1/Relu:0"),
-	# graph.get_tensor_by_name("scale2/MaxPool:0"),
-	# graph.get_tensor_by_name("scale2/block1/Relu:0"),
-	# graph.get_tensor_by_name("scale2/block2/Relu:0"),
-	# graph.get_tensor_by_name("scale2/block3/Relu:0"),
-	# graph.get_tensor_by_name("scale3/block1/Relu:0"),
-	# graph.get_tensor_by_name("scale5/block3/Relu:0"),
-	graph.get_tensor_by_name("avg_pool:0"),
-	# graph.get_tensor_by_name("prob:0"),
-]
-o = sess.run(i, {images: img[np.newaxis, :]})
-print o[0], o[0].shape
+before_fc_pool = graph.get_tensor_by_name("avg_pool:0"),
+img_features = sess.run(before_fc_pool, {images: img[np.newaxis, :]})
+print img_features[0], img_features[0].shape
