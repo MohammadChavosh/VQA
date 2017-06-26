a6e12a16d7135cd3fa51602ac21d5303fdc0eb2b
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-06-26 16:33:25 +0430

    change articheture

--- train.py
+++ train.py
@@ -163,9 +163,12 @@ def run():
     output_len = len(answers_vocab_processor.vocabulary_)
     output_answers = tf.placeholder(tf.float32, [None, output_len], name="output_answers")
 
-    q_to_img_w = tf.Variable(tf.random_normal([n_hidden, pre_output_len]), name="q_to_img_w")
-    q_to_img_bias = tf.Variable(tf.random_normal([pre_output_len]), name="q_to_img_bias")
-    img_out_w = tf.matmul(questions_features, q_to_img_w) + q_to_img_bias
+    # tmp_len = img_features_len * pre_output_len
+    # q_to_img_w = tf.Variable(tf.random_normal([n_hidden, tmp_len]), name="q_to_img_w")
+    # q_to_img_bias = tf.Variable(tf.random_normal([tmp_len]), name="q_to_img_bias")
+    # img_out_w = tf.matmul(questions_features, q_to_img_w) + q_to_img_bias
+    # img_out_w = tf.reshape(img_out_w, [-1, img_features_len, pre_output_len])
+    img_out_w = tf.Variable(tf.random_normal([img_features_len, pre_output_len]), name="img_w")
     q_out_w = tf.Variable(tf.random_normal([n_hidden, pre_output_len]), name="q_out_w")
     out_bias = tf.Variable(tf.random_normal([pre_output_len]), name="out_bias")
 
