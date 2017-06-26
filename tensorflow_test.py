7fddf58d26ea2590b1ed8c17dc4bbba97704cc0b
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-06-26 20:28:45 +0430

    test

--- tensorflow_test.py
+++ tensorflow_test.py
@@ -1,7 +1,10 @@
 from train import get_batch, load_related_train_data, load_train_data
 
-step = 520
+x = 520
 questions_vocab_processor, answers_vocab_processor, max_question_length = load_related_train_data()
 questions, answers, images_paths = load_train_data(questions_vocab_processor, answers_vocab_processor)
-batch_in_questions, batch_in_images, batch_out = get_batch(step, questions, answers, images_paths, len(answers_vocab_processor.vocabulary_))
-
+for i in range(10):
+    step = x + i
+    print step
+    batch_in_questions, batch_in_images, batch_out = get_batch(step, questions, answers, images_paths, len(answers_vocab_processor.vocabulary_))
+    print '================'
