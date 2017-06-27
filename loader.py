854428406e11752ec362c4eba59fb88ff0a131c4
Seyed Mohammad Chavoshian <mohammad.chavosh@gmail.com>
2017-06-27 11:32:36 +0430

    add loader file

--- loader.py
+++ loader.py
@@ -118,62 +118,16 @@ def run():
     questions, answers, images_paths = load_data(questions_vocab_processor, answers_vocab_processor, True)
 
     sess = tf.Session()
-    saver = tf.train.import_meta_graph('data/tensorflow-resnet-pretrained-20160509/ResNet-L152.meta')
-    saver.restore(sess, 'data/tensorflow-resnet-pretrained-20160509/ResNet-L152.ckpt')
+    saver = tf.train.import_meta_graph('data/tensorflow-resnet-pretrained-20160509/vqa_model-5000.meta')
+    saver.restore(sess, tf.train.latest_checkpoint('./'))
 
     graph = tf.get_default_graph()
+    input_questions = graph.get_tensor_by_name('input_questions:0')
     images = graph.get_tensor_by_name("images:0")
-    raw_img_features = graph.get_tensor_by_name("avg_pool:0")
-    raw_to_img_features_w = tf.Variable(tf.random_normal([raw_img_features.shape.as_list()[1], img_features_len]),
-                                        name="raw_to_img_w")
-    raw_to_img_features_bias = tf.Variable(tf.random_normal([img_features_len]), name="raw_to_img_bias")
-    img_features = tf.nn.relu(tf.matmul(raw_img_features, raw_to_img_features_w) + raw_to_img_features_bias)
-
-    embedding_w = tf.Variable(tf.random_uniform([len(questions_vocab_processor.vocabulary_), embedding_dim], -1.0, 1.0), name="embedding_w")
-    input_questions = tf.placeholder(tf.int32, [None, questions.shape[1]], name="input_questions")
-    embedded_chars = tf.nn.embedding_lookup(embedding_w, input_questions)
-    unstacked_embedded_chars = tf.unstack(embedded_chars, max_question_length, 1)
-    lstm_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-    encoded_questions, _ = rnn.static_rnn(lstm_cell, unstacked_embedded_chars, dtype=tf.float32)
-    q_w = tf.Variable(tf.random_normal([n_hidden, n_hidden]), name="q_w")
-    q_bias = tf.Variable(tf.random_normal([n_hidden]), name="q_bias")
-    questions_features = tf.nn.relu(tf.matmul(encoded_questions[-1], q_w) + q_bias)
-
-    output_len = len(answers_vocab_processor.vocabulary_)
-    output_answers = tf.placeholder(tf.float32, [None, output_len], name="output_answers")
-
-    # tmp_len = img_features_len * pre_output_len
-    # q_to_img_w = tf.Variable(tf.random_normal([n_hidden, tmp_len]), name="q_to_img_w")
-    # q_to_img_bias = tf.Variable(tf.random_normal([tmp_len]), name="q_to_img_bias")
-    # img_out_w = tf.matmul(questions_features, q_to_img_w) + q_to_img_bias
-    # img_out_w = tf.reshape(img_out_w, [-1, img_features_len, pre_output_len])
-    img_out_w = tf.Variable(tf.random_normal([img_features_len, pre_output_len]), name="img_w")
-    q_out_w = tf.Variable(tf.random_normal([n_hidden, pre_output_len]), name="q_out_w")
-    out_bias = tf.Variable(tf.random_normal([pre_output_len]), name="out_bias")
-
-    pre_output = tf.nn.relu(tf.matmul(img_features, img_out_w) + tf.matmul(questions_features, q_out_w) + out_bias)
-    pre_output_w = tf.Variable(tf.random_normal([pre_output_len, output_len]), name="pre_out_w")
-    pre_output_bias = tf.Variable(tf.random_normal([output_len]), name="pre_out_bias")
-
-    prediction = tf.matmul(pre_output, pre_output_w) + pre_output_bias
-    prediction = tf.identity(prediction, name="prediction")
-    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction, labels=output_answers), name='cost')
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    output_answers = graph.get_tensor_by_name('output_answers:0')
+    cost = graph.get_tensor_by_name('cost:0')
 
     with sess.as_default():
-        sess.run(tf.global_variables_initializer())
-        init_embedding_w = load_word2vec(questions_vocab_processor)
-        sess.run(embedding_w.assign(init_embedding_w))
-        step = 0
-        while step < training_iters:
-            batch_in_questions, batch_in_images, batch_out = get_batch(step, questions, answers, images_paths, len(answers_vocab_processor.vocabulary_))
-            sess.run(optimizer, feed_dict={input_questions: batch_in_questions, images: batch_in_images, output_answers: batch_out})
-            if step % display_step == 0:
-                loss = sess.run(cost, feed_dict={input_questions: batch_in_questions, images: batch_in_images, output_answers: batch_out})
-                print("Iter " + str(step) + ", Minibatch Loss= " + "{:.6f}".format(loss))
-            step += 1
-        print("Optimization Finished!")
-        saver.save(sess, 'vqa_model',global_step=step)
 
         step = 0
         losses = []
